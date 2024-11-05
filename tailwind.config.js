151d54adcdb0f609242c848ab6906fcef7ffa784
wldnd9904 <wlwdnd9904@uos.ac.kr>
2024-11-06 08:46:52 +0900

    chore: 린트,  테일윈드 설정 조금더

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./index.html", // HTML 파일
-    "./src/**/*.{js,jsx,ts,tsx}", // React와 TypeScript 파일
-    "./.storybook/**/*.ts", // Storybook 관련 파일도 추가
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "../.storybook/*"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        iPhone: "390px",
+      },
+      width: {
+        iPhone: "390px",
+      },
+      height: {
+        iPhone: "844px",
+      },
+      backgroundImage: {
+        smartthings:
+          "radial-gradient(circle at top right, #3072A5 0%, #84A8D1 47%, #95B1DF 67%, #9BA1C9 100%)",
+      },
+      fontFamily: {
+        samsungSharpSans: ["SamsungSharpSans", "sans-serif"], // 폰트 이름을 지정
+      },
+    },
   },
   plugins: [],
 };
